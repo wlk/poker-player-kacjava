b10612eefabdf09d44916af28afed83116396180
Paweł Bilewicz <pawel.biewicz@unity.pl>
2016-03-06 11:20:38 +0100

    Test wersji

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,16 +2,14 @@ package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
 
-import java.util.Map;
-
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+	static final String VERSION = "KacJava-" + 1;
 
-    public static int betRequest(JsonElement request) {
-        return 1000;
-    }
+	public static int betRequest(JsonElement request) {
+		return 1000;
+	}
 
-    public static void showdown(JsonElement game) {
-    }
+	public static void showdown(JsonElement game) {
+	}
 }
