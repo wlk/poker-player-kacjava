cd3770b48bec0843076694f674c106411502ddf7
Grzegorz Sobczyk <gsobczyk@gmail.com>
2016-03-06 15:37:39 +0100

    wersja 9

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,13 +9,13 @@ import com.google.gson.JsonElement;
 
 public class Player {
 
-	static final String VERSION = "KacJava-s" + "8.1";
+	static final String VERSION = "KacJava-s" + "9";
 
     private static Gson gson = new Gson();
 
 	public static int betRequest(JsonElement request) {
 
-        Strategy strategy = new ExceptionCatcherStrategy(new Strategy8());
+        Strategy strategy = new ExceptionCatcherStrategy(new Strategy9());
 
         Optional<GameStateDto> dtoOption = parseJson(request);
         return dtoOption
