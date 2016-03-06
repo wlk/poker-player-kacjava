879d16d55a9b7f055c1aa747add1369785446ac7
mielcarek.pawel@gmail.com <KochamTenBit1@>
2016-03-06 11:46:14 +0100

    parsowanie jsona

--- src/main/java/org/leanpoker/player/dto/GameStateDto.java
+++ src/main/java/org/leanpoker/player/dto/GameStateDto.java
@@ -2,15 +2,46 @@ package org.leanpoker.player.dto;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 public class GameStateDto {
 
     @SerializedName("minimum_raise")
     private int minimumRaise;
+
+    @SerializedName("bet_index")
+    private int betIndex;
+
+    @SerializedName("current_buy_in")
+    private int currentBuyIn;
+
+    private List<PlayerDto> players;
 
     public int getMinimumRaise() {
         return minimumRaise;
     }
     public void setMinimumRaise(int minimumRaise) {
         this.minimumRaise = minimumRaise;
+    }
+
+    public int getBetIndex() {
+        return betIndex;
+    }
+    public void setBetIndex(int betIndex) {
+        this.betIndex = betIndex;
+    }
+
+    public int getCurrentBuyIn() {
+        return currentBuyIn;
+    }
+    public void setCurrentBuyIn(int currentBuyIn) {
+        this.currentBuyIn = currentBuyIn;
+    }
+
+    public List<PlayerDto> getPlayers() {
+        return players;
+    }
+    public void setPlayers(List<PlayerDto> players) {
+        this.players = players;
     }
 }
